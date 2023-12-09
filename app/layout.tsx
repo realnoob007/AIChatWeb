6143bef191c2486675b7f3e66368d36f405623e1
Sophia Patel <87698941+realnoob007@users.noreply.github.com>
2023-12-09 22:51:37 +0800

    Update Website Name

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "AI Chat Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Learnit",
+  description: "Your AI Learnining Tutor",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "AI Chat Web",
+    title: "Learnit",
     statusBarStyle: "default",
   },
 };
