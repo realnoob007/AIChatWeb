cf1cf247d46157c96d2379067df8019ede67266b
Sophia Patel <87698941+realnoob007@users.noreply.github.com>
2023-12-09 22:52:52 +0800

    Delete default system prompt

--- app/constant.ts
+++ app/constant.ts
@@ -67,8 +67,4 @@ export const OpenaiPath = {
 };
 
 export const DEFAULT_INPUT_TEMPLATE = `{{input}}`; // input / time / model / lang
-export const DEFAULT_SYSTEM_TEMPLATE = `
-You are ChatGPT, a large language model trained by OpenAI.
-Knowledge cutoff: 2021-09
-Current model: {{model}}
-Current time: {{time}}`;
+export const DEFAULT_SYSTEM_TEMPLATE = ``;
